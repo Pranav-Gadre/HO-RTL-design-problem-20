cddd7425ffd3df02209ce4193d52cacc2cd78dc5
Pranav <pranav1gadre@gmail.com>
2026-02-25 08:19:02 +0530

    commit 4: tab to spaces.

--- async_reset.v
+++ async_reset.v
@@ -8,49 +8,49 @@ module async_reset (
 );
 
   // Write your logic here...
-	reg [4:0] count;
-	reg [4:0] count_store;
-	reg       release_reset;
-	reg       gate_clk;
-	
-	assign release_reset_o = release_reset;
-	assign gate_clk_o      = gate_clk;
-	
-	always @ (posedge clk or posedge reset) begin 
-		if (reset) begin 
-			count_store <= 0;
-		end else begin 
-			count_store <= count;
-		end 
-	end
-	
-	always @ (*) begin  
-		if (count_store < 20) 
-			count = count_store + 1;
-		else 
-			count = count_store;
-	end 
-	
-	always @ (*) begin 
-		if (count >= 11) begin 
-			release_reset = 1;
-		end else if (count < 11) begin 
-			release_reset = 0;
-		end else begin 
-			release_reset = 1;
-		end 
-	end 
-	
-	always @ (*) begin 
-		if (count < 5) begin 
-			gate_clk = 0;
-		end else if (count < 18) begin 
-			gate_clk = 1;
-		end else if (count >= 18) begin 
-			gate_clk = 0;
-		end else begin 
-			gate_clk = 0;
-		end 
-	end 
+  reg [4:0] count;
+  reg [4:0] count_store;
+  reg       release_reset;
+  reg       gate_clk;
+  
+  assign release_reset_o = release_reset;
+  assign gate_clk_o      = gate_clk;
+  
+  always @ (posedge clk or posedge reset) begin 
+    if (reset) begin 
+      count_store <= 0;
+    end else begin 
+      count_store <= count;
+    end 
+  end
+  
+  always @ (*) begin  
+    if (count_store < 20) 
+      count = count_store + 1;
+    else 
+      count = count_store;
+  end 
+  
+  always @ (*) begin 
+    if (count >= 11) begin 
+      release_reset = 1;
+    end else if (count < 11) begin 
+      release_reset = 0;
+    end else begin 
+      release_reset = 1;
+    end 
+  end 
+  
+  always @ (*) begin 
+    if (count < 5) begin 
+      gate_clk = 0;
+    end else if (count < 18) begin 
+      gate_clk = 1;
+    end else if (count >= 18) begin 
+      gate_clk = 0;
+    end else begin 
+      gate_clk = 0;
+    end 
+  end 
 
 endmodule
